5b30b6accd1c6016ad5c8480827c0d3744178b21
sanukin39 <sanuki@sanukikounoMac-mini.local>
2019-02-21 22:57:02 +0900

    Add Unity Project

---
+++ Assets/Example/Example.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Example : MonoBehaviour
+{
+    [SerializeField] private InputField _vibrateMillisecondsInputField;
+    [SerializeField] private Button _vibrateButton;
+
+    void Awake()
+    {
+        _vibrateButton.onClick.AddListener(Vibrate);
+    }
+
+    void Vibrate()
+    {
+        var milliseconds = int.Parse(_vibrateMillisecondsInputField.text);
+        UniAndroidVibration.Vibrate(milliseconds);
+    }
+}
---
+++ Assets/UniAndroidVibration/UniAndroidVibration.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UniAndroidVibration
+{
+    private const string PackageClassPath = "net.sanukin.vibration.UniVibration";
+    
+    public static void Vibrate(int milliseconds)
+    {
+#if !UNITY_EDITOR && UNITY_ANDROID
+        var javaClass = new AndroidJavaClass(PackageClassPath);
+        javaClass.CallStatic("vibrate", milliseconds);
+#endif
+    }
+}
