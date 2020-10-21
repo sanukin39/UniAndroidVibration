dcac946442149bf025ea790064f7ae0a9c146b47
sanukin39 <ericsson.goodjob@gmail.com>
2020-10-21 13:36:53 +0900

    Cache android java class object

--- Assets/UniAndroidVibration/UniAndroidVibration.cs
+++ Assets/UniAndroidVibration/UniAndroidVibration.cs
@@ -3,12 +3,18 @@
 public class UniAndroidVibration
 {
     private const string PackageClassPath = "net.sanukin.vibration.UniVibration";
-    
+#if !UNITY_EDITOR && UNITY_ANDROID
+    private static AndroidJavaClass _javaClass;
+#endif
+
     public static void Vibrate(int milliseconds)
     {
 #if !UNITY_EDITOR && UNITY_ANDROID
-        var javaClass = new AndroidJavaClass(PackageClassPath);
-        javaClass.CallStatic("vibrate", milliseconds);
+        if (_javaClass == null)
+        {
+            _javaClass = new AndroidJavaClass(PackageClassPath);
+        }
+        _javaClass.CallStatic("vibrate", milliseconds);
 #endif
     }
 }
